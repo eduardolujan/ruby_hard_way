495426921249b7892ba08981e695a4f2b3b566de
Eduardo Lujan <eduardo.lujan.p@gmail.com>
2017-11-07 08:23:32 -0600

    Added: New files and gitignore

---
+++ ex1.rb
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+puts "Hello world!"
+puts "Hello world Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Priting."
+puts "I'd must rather you 'not'."
+puts 'I "said" do not touch this.'
